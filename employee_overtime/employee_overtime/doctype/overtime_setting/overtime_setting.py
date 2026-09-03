8f7340046f8231d5a8d1d80627a843ec8a59994e
Arshpreet Sing Sandhu <110418427+Sandhuarsh@users.noreply.github.com>
2026-09-03 13:36:03 +0530

    Delete employee_overtime directory

--- employee_overtime/employee_overtime/doctype/overtime_setting/overtime_setting.py
+++
@@ -1,8 +0,0 @@
-import frappe
-from frappe.model.document import Document
-
-
-class OvertimeSetting(Document):
-    def validate(self):
-        if self.days_divisor is not None and self.days_divisor <= 0:
-            frappe.throw("Days Divisor must be greater than zero.")
